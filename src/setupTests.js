9825519a03983e736e29219445a517b842a1f0f9
kridsada <vim_intason@hotmail.com>
2026-02-25 19:23:53 +0700

    test: add mock localStorage implementation to setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,29 @@
 import '@testing-library/jest-dom';
+
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value.toString();
+    },
+    clear: () => {
+      store = {};
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    get length() {
+      return Object.keys(store).length;
+    },
+    key: (index) => {
+      const keys = Object.keys(store);
+      return keys[index] || null;
+    }
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+  writable: true
+});
